e446a7faf3393b8d01c257f96f109637aba741a1
bazinga <linsunny2005@gmail.com>
2022-11-14 11:23:18 +0800

    [add] dhcp release [modify] get server ip

--- dhcp_client.py
+++ dhcp_client.py
@@ -1,7 +1,9 @@
 from scapy.all import *
+import time
 
-src_mac =   '00:01:02:03:04:0c'
-interface = 'ens33'
+src_mac =   '00:01:02:03:04:07'
+interface = 'enx503eaabb51e7'
+#interface = 'ens33'
 
 print('src_mac = ' + src_mac)
 
@@ -17,18 +19,18 @@ sendp(packet, iface = interface, verbose = 0)
 
 # Receive Offer
 received_offer = sniff(iface = interface, filter = 'port 68 and port 67', stop_filter=lambda pkt: BOOTP in pkt and pkt[BOOTP].op == 2 and pkt[DHCP].options[0][1] == 2, timeout=5)
-bootp_reply = received_offer[0]['BOOTP']
+#bootp_reply = received_offer[0]['BOOTP']
 
-print('ser_mac = ' + received_offer[0]['Ether'].src)
-print('ser_ip = ' + bootp_reply.siaddr)
-print('offered_ip = ' + bootp_reply.yiaddr)
+server_mac = received_offer[0]['Ether'].src
+server_ip = received_offer[0]['IP'].src
+offered_ip = received_offer[0]['BOOTP'].yiaddr
 
 # Send Request
 ethernet=   Ether(dst='ff:ff:ff:ff:ff:ff',src=src_mac)
 ip      =   IP(src ='0.0.0.0',dst='255.255.255.255')
 udp     =   UDP(sport=68,dport=67)
 bootp   =   BOOTP(chaddr = mac2str(src_mac),xid =  0x01020304,flags= 0x0)
-dhcp    =   DHCP(options=[('message-type','request'),('client_id', src_mac),('requested_addr', bootp_reply.yiaddr),('server_id', bootp_reply.siaddr),'end'])
+dhcp    =   DHCP(options=[('message-type','request'),('client_id', src_mac),('requested_addr', offered_ip),('server_id', server_ip),'end'])
 packet  =   ethernet / ip / udp / bootp / dhcp
 
 sendp(packet, iface = interface, verbose = 0)
@@ -38,9 +40,22 @@ received_ack = sniff(iface = interface, filter="port 68 and port 67", stop_filte
 
 print('DHCP finished!')
 
+print('Start sleeping')
+time.sleep(5)
+print('End sleeping')
 
 
+# Release
+#ethernet=   Ether(dst=server_mac,src=src_mac)
+ip      =   IP(src=offered_ip, dst=server_ip)
+udp     =   UDP(sport=68,dport=67)
+bootp   =   BOOTP(chaddr = mac2str(src_mac),xid =  0x01020304)
+dhcp    =   DHCP(options=[('message-type','release'),'end'])
+packet  =   ethernet / ip / udp / bootp / dhcp
 
+sendp(packet, iface = interface, verbose = 0)
+
+print('IP released!')
 
 
 
